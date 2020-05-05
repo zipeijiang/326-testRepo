a0cb7ddb4dfb0d8f015da200182bf3ec088664eb
unknown <zipeijiang@umass.edu>
2020-05-04 20:16:49 -0400

    xxxx

--- main_sql.js
+++ main_sql.js
@@ -1,9 +1,9 @@
 'use strict';
-Object.defineProperty(exports, "__esModule", { value: true });
+exports.__esModule = true;
 const database_sql_1 = require("./database_sql");
 const server_sql_1 = require("./server_sql");
 const theDatabase = new database_sql_1.Database('wojhwndc'); // CHANGE THIS
 console.log("database OK");
 const theServer = new server_sql_1.Server(theDatabase);
 console.log("Server OK");
-theServer.listen(8080);
+theServer.listen(process.env.PORT || 8080);
